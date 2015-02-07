56e27f56b18ffc5f6017bd5a481ca1a16e1ff6a4
Sinistrus <sahel.khan@gmail.com>
2015-02-07 00:49:32 -0500

    confirmed works on PC. now sending it bad to tablet in another commit.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 	
 	//testing to determine that build paths still work
+	//tested from tablet to PC, still works. Now must test from PC to tablet.
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
