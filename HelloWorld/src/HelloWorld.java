5c3cdc64241d7b1f7b7782852cd1eca8a123296c
Sinistrus <sahel.khan@gmail.com>
2015-02-08 20:17:49 -0500

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,12 +1,7 @@
 
 
 public class HelloWorld {
-	//02-08-15 branch update test
-	//testing to determine that build paths still work
-	//tested from tablet to PC, still works. Now must test from PC to tablet.
-	//still works so far. Great!
-	//Final test
-	//almost done
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
