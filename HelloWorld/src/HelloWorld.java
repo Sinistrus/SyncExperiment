56eacc7d133888d585dc08ca35b2f184cdbe0c87
Sinistrus <sahel.khan@gmail.com>
2015-02-08 19:29:41 -0500

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,7 @@
 
-//02-08-15 branch update test
+
 public class HelloWorld {
-	
+	//02-08-15 branch update test
 	//testing to determine that build paths still work
 	//tested from tablet to PC, still works. Now must test from PC to tablet.
 	//still works so far. Great!
